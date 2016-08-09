b0eaa1b942330ba34eb2bf768ed7cfbe33350645
Vad <vad.adler@gmail.com>
2016-08-09 11:21:12 -0700

    Added code to show values on bar chart.

--- storagescanner/app/src/main/java/com/odinarts/android/storagescanner/chart/HorizontalBarChartActivity.java
+++ storagescanner/app/src/main/java/com/odinarts/android/storagescanner/chart/HorizontalBarChartActivity.java
@@ -2,7 +2,6 @@
 package com.odinarts.android.storagescanner.chart;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +10,6 @@ import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
@@ -38,8 +36,6 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
         OnChartValueSelectedListener {
 
     protected HorizontalBarChart mChart;
-    private SeekBar mSeekBarX, mSeekBarY;
-    private TextView tvX, tvY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,37 +47,19 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
         // Get data.
         ArrayList<FileData> data = this.getIntent().getParcelableArrayListExtra("data");
 
-        tvX = (TextView) findViewById(R.id.tvXMax);
-        tvY = (TextView) findViewById(R.id.tvYMax);
-
-        //mSeekBarX = (SeekBar) findViewById(R.id.seekBar1);
-        //mSeekBarY = (SeekBar) findViewById(R.id.seekBar2);
-
         mChart = (HorizontalBarChart) findViewById(R.id.chart1);
         mChart.setOnChartValueSelectedListener(this);
-        // mChart.setHighlightEnabled(false);
 
         mChart.setDrawBarShadow(false);
-
+        mChart.setDrawValueAboveBar(true);
+        mChart.setDescription("10 Largest Files");
+        mChart.setMaxVisibleValueCount(100);
+        mChart.setPinchZoom(false);
+        mChart.setDrawGridBackground(false);
         mChart.setDrawValueAboveBar(true);
 
-        mChart.setDescription("");
-
-        // if more than 60 entries are displayed in the chart, no values will be
-        // drawn
-        mChart.setMaxVisibleValueCount(10);
-
-        // scaling can now only be done on x- and y-axis separately
-        mChart.setPinchZoom(false);
-
-        // draw shadows for each bar that show the maximum value
-        // mChart.setDrawBarShadow(true);
-
-        mChart.setDrawGridBackground(false);
-        mChart.setDrawValueAboveBar(false);
-
         XAxis xl = mChart.getXAxis();
-        xl.setPosition(XAxisPosition.BOTTOM);
+        xl.setPosition(XAxisPosition.BOTTOM_INSIDE);
         xl.setTypeface(mTfLight);
         xl.setDrawAxisLine(false);
         xl.setDrawGridLines(false);
@@ -101,17 +79,9 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
         //yr.setAxisMinValue(0f); // this replaces setStartAtZero(true)
 //        yr.setInverted(true);
 
-        //setData(10, 50);
         setData(data);
         mChart.setFitBars(true);
         mChart.animateY(2500);
-
-        // setting data
-        //mSeekBarY.setProgress(50);
-        //mSeekBarX.setProgress(12);
-
-        //mSeekBarY.setOnSeekBarChangeListener(this);
-        //mSeekBarX.setOnSeekBarChangeListener(this);
 
         Legend l = mChart.getLegend();
         l.setPosition(LegendPosition.BELOW_CHART_LEFT);
@@ -198,12 +168,13 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
+/*
         tvX.setText("" + (mSeekBarX.getProgress() + 1));
         tvY.setText("" + (mSeekBarY.getProgress()));
 
         setData(mSeekBarX.getProgress() + 1, mSeekBarY.getProgress());
         mChart.setFitBars(true);
+*/
         mChart.invalidate();
     }
 
@@ -223,19 +194,33 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
         float barWidth = 9f;
         float spaceForBar = 10f;
 
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        ArrayList<BarDataSet> sets = new ArrayList<BarDataSet>();
+        ArrayList<ArrayList<BarEntry>> entries = new ArrayList<ArrayList<BarEntry>>();
+        ArrayList<String> names = new ArrayList<String>();
 
         for (int i = 0; i < data.size(); i++) {
-            yVals1.add(new BarEntry(i * spaceForBar, data.get(i).getLength()));
+            ArrayList<BarEntry> e = new ArrayList<BarEntry>();
+            e.add(0, new BarEntry(i * spaceForBar, data.get(i).getLength()));
+            entries.add(i, e);
+            names.add(data.get(i).getName());
         }
 
-        BarDataSet set1 = new BarDataSet(yVals1, "10 largest files");
-        set1.setDrawValues(true);
-        set1.setValueTextColor(Color.BLACK);
-        set1.setValueTextSize(12f);
+        for (int i = 0; i < data.size(); i++) {
+            sets.add(i, new BarDataSet(entries.get(i), names.get(i)));
+        }
+
+
+        //BarDataSet set1 = new BarDataSet(barEntry, "haha");
+        //set1.setDrawValues(true);
+        //set1.setValueTextColor(Color.BLACK);
+        //set1.setValueTextSize(12f);
 
         ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
-        dataSets.add(set1);
+        for (int i = 0; i < data.size(); i++) {
+            dataSets.add(sets.get(i));
+        }
+
+        //dataSets.add(set1);
 
         BarData bData = new BarData(dataSets);
         bData.setValueTextSize(10f);
@@ -246,7 +231,6 @@ public class HorizontalBarChartActivity extends DemoBase implements OnSeekBarCha
         mChart.getAxisLeft().setDrawLabels(false);
         mChart.getAxisRight().setDrawLabels(false);
         mChart.getXAxis().setDrawLabels(false);
-
         mChart.getLegend().setEnabled(false);   // Hide the legend
     }
 
