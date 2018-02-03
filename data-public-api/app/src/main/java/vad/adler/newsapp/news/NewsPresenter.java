7146a36ff4e80e36dbfeece9877951a390bc2753
vad <vad.adler@gmail.com>
2018-02-03 12:50:52 -0800

    Added skeleton implementation of NewsPresenter.

---
+++ data-public-api/app/src/main/java/vad/adler/newsapp/news/NewsPresenter.java
@@ -0,0 +1,13 @@
+package vad.adler.newsapp.news;
+
+/**
+ *
+ */
+
+public class NewsPresenter implements NewsContract.Presenter {
+    @Override
+    public void subscribe() {}
+
+    @Override
+    public void unsubscribe() {}
+}
