3cbf35940decc302c89300dc75dd36e908a6e0d3
vad <vad.adler@gmail.com>
2018-02-07 12:27:34 -0800

    Added Dagger dependency. Defined injectable objects.

---
+++ data-public-api/app/src/main/java/vad/adler/newsapp/di/NetworkModule.java
@@ -0,0 +1,41 @@
+package vad.adler.newsapp.di;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import vad.adler.newsapp.Constants;
+import vad.adler.newsapp.data.NewsApi;
+
+/**
+ * DI: provide objects which can be injected.
+ */
+
+public class NetworkModule {
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        return new GsonBuilder().create();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson) {
+        return new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(Constants.BASE_URL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    NewsApi provideCityApi(Retrofit retrofit) {
+        return retrofit.create(NewsApi.class);
+    }
+}
